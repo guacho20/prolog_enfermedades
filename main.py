e3ea3020fdbf1b3809c430e6ab1d1ac651555e23
Jhon Guacho <jhon_199713@hotmail.com>
2023-07-31 21:16:45 -0500

    adding new functionalities

--- main.py
+++ main.py
@@ -17,26 +17,38 @@ def Index():
 
 @app.route("/test")
 def test():
-    return render_template("test.html", data=[])
+    datos = format_data(list(prolog.query("sintomas(X)")))
+    return render_template("test.html", data=datos)
 
 @app.route("/diseases")
 def diseases():
-    datos = list(prolog.query("enfermedades(X)"))
-    diseases = []
-    for fila in datos:
-        data =fila['X'].replace("row(","").replace(")","").split(',')
-        diseases.append(data)
-    return render_template("diseases.html", data=diseases)
+    datos = format_data(list(prolog.query("enfermedades(X)")))
+    return render_template("diseases.html", data=datos)
 
 @app.route("/symptoms")
 def symptoms():
-    datos = list(prolog.query("sintomas(X)"))
-    symptoms = []
-    for fila in datos:
-        data =fila['X'].replace("row(","").replace(")","").split(',')
-        symptoms.append(data)
-    return render_template("symptoms.html", data=symptoms)
+    datos = format_data(list(prolog.query("sintomas(X)")))
+    return render_template("symptoms.html", data=datos)
 
+@app.route("/diseasebysymptom")
+def diseasebysymptom():
+
+    diseases = format_data(list(prolog.query("enfermedades(X)")))
+    symptoms = format_data(list(prolog.query("sintomas(X)")))
+    diseasebysymptom = format_data(list(prolog.query("enfermedad_sintomas(X)")))
+    
+    return render_template("diseasebysymptom.html", data=[diseases, symptoms, diseasebysymptom ])
+
+@app.route("/diseasebytreatment")
+def diseasebytreatment():
+
+    diseases = format_data(list(prolog.query("enfermedades(X)")))
+    treatments = format_data(list(prolog.query("tratamientos(X)")))    
+    
+    return render_template("diseasebytreatment.html", data=[diseases,treatments])
+
+
+#METODO POST
 @app.route("/diseases", methods=["POST"])
 def add_diseases():
     if request.method == "POST":
@@ -44,7 +56,7 @@ def add_diseases():
 
         print(name)
 
-        list(prolog.query("es_enfermedad(" + name + ",X)"))
+        list(prolog.query("es_enfermedad(" + replace_space_by_underscore(name) + ",X)"))
   
         flash("Added successfully")
 
@@ -57,11 +69,75 @@ def add_symptoms():
 
         print(name)
 
-        list(prolog.query("es_sintoma(" + name + ",X)"))
+        list(prolog.query("es_sintoma(" + replace_space_by_underscore(name) + ",X)"))
   
         flash("Added successfully")
 
         return redirect(url_for("symptoms"))
+    
+@app.route("/diseasebysymptom", methods=["POST"])
+def add_diseasebysymptom():
+    if request.method == "POST":
+        enfermedad = request.form["enfermedad"]
+        sintoma = request.form["sintoma"]
+
+        print(enfermedad, sintoma)
+
+        list(prolog.query("tiene_sintoma("+enfermedad+"," + sintoma + ",X)"))
+  
+        flash("Added successfully")
+
+        return redirect(url_for("diseasebysymptom"))
+
+@app.route("/diseasebytreatment", methods=["POST"])
+def add_diseasebytreatment():
+    if request.method == "POST":
+        enfermedad = request.form["enfermedad"]
+        tratamiento = request.form["tratamiento"]
+
+        print(enfermedad, tratamiento)
+
+        list(prolog.query("tiene_tratamiento("+enfermedad+"," + replace_space_by_underscore(tratamiento) + ",X)"))
+  
+        flash("Added successfully")
+
+        return redirect(url_for("diseasebytreatment"))
+
+@app.route("/test", methods=["POST"])
+def diagnostic():
+    if request.method == "POST":
+        #sintomas = request.form["sintomas"]
+        frutas = request.form.getlist('frutas[]')
+        print(len(frutas))
+        if len(frutas) == 0:
+           flash("Seleccione los sintomas para diagnosticar", "error")
+           return redirect(url_for("test"))
+        
+        print(frutas)
+        print(', '.join(frutas))
+        print( 'Las Frutas seleccionadas son: ' + ', '.join(frutas))
+        #print(sintomas)
+        """ tratamiento = request.form["tratamiento"]
+
+
+        list(prolog.query("tiene_tratamiento("+enfermedad+"," + replace_space_by_underscore(tratamiento) + ",X)")) """
+  
+        flash("Added successfully", 'info')
+
+
+        return redirect(url_for("test"))
+
+#Funtions
+def format_data(datos):
+    newdata = []
+    for fila in datos:
+        data =fila['X'].replace("row(","").replace(")","").split(',')
+        newdata.append(data)
+    return newdata
+
+def replace_space_by_underscore(text):
+    name = text.replace(" ","_")
+    return name.lower()
 
 # starting the app
 if __name__ == "__main__":
--- program.pl
+++ program.pl
@@ -18,6 +18,18 @@ es_sintoma(A,X) :-
                 'INSERT INTO symptoms(name) VALUES (\'~w\')'-[A],
                 affected(X)).
 
+tiene_sintoma(A,B,X):-
+    abrir_conexion,
+    odbc_query('postgresql',
+                'INSERT INTO disease_symptoms(disease_id,symptom_id) VALUES (\'~w\',\'~w\')'-[A,B],
+                affected(X)).
+
+tiene_tratamiento(A,B,X):-
+    abrir_conexion,
+    odbc_query('postgresql',
+                'INSERT INTO treatments(disease_id, name) VALUES (\'~w\',\'~w\')'-[A,B],
+                affected(X)).
+
 enfermedades(X):-
     abrir_conexion,
     odbc_query('postgresql','SELECT id, name FROM diseases ORDER BY id DESC ', X). 
@@ -26,4 +38,18 @@ sintomas(X):-
     abrir_conexion,
     odbc_query('postgresql','SELECT id, name FROM symptoms', X). 
 
-    
+enfermedad_sintomas(X):-
+    abrir_conexion,
+    odbc_query('postgresql','select a.id, b.name as enfermedad,c.name as sintoma 
+                from disease_symptoms a
+                join diseases b on a.disease_id = b.id
+                join symptoms c on a.symptom_id = c.id', X).
+
+tratamientos(X):-
+    abrir_conexion,
+    odbc_query('postgresql','select a.id,b.name as enfermedad,a.name as tratamiento 
+                from treatments a 
+                join diseases b on a.disease_id = b.id', X).
+
+
+                
